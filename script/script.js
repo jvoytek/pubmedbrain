364c7ea22b2398c7b17d712584fb652d1e6d514c
jvoytek <jessica.voytek@gmail.com>
2011-01-27 15:08:50 -0800

    Reformatted Javascript and added .gitignore

--- script/script.js
+++ script/script.js
@@ -198,141 +198,126 @@ function tree(json){
 
 
 function makeGraph(json){
-  $("#infovis").empty();
-  fd = new $jit.ForceDirected({  
-  //id of the visualization container  
-  injectInto: 'infovis',  
-  //Enable zooming and panning  
-  //by scrolling and DnD  
-  Navigation: {  
-    enable: true,  
-    //Enable panning events only if we're dragging the empty  
-    //canvas (and not a node).  
-    panning: 'avoid nodes',  
-    zooming: 10 //zoom speed. higher is more sensible  
-  },  
-  // Change node and edge styles such as  
-  // color and width.  
-  // These properties are also set per node  
-  // with dollar prefixed data-properties in the  
-  // JSON structure.  
-  Node: {  
-    overridable: true  
-  },  
-  Edge: {  
-    overridable: true,  
-    color: '#23A4FF',  
-    lineWidth: 0.4  
-  },  
-  //Native canvas text styling  
-  Label: {  
-    type: labelType, //Native or HTML  
-    size: 16,  
-    style: 'normal',
-    color: '#000'
-  },  
-  //Add Tips  
-  Tips: {  
-    enable: true,  
-    onShow: function(tip, node) {  
-      //count connections  
-      var count = 0;  
-      node.eachAdjacency(function() { count++; });  
-      //display node info in tooltip  
-      tip.innerHTML = "<div class=\"tip-text\"><b>Category:</b> " + node.data.category + "<br /><b>Prob. association with search term:</b> " + node.data.prob_association + "</div>";  
-    }  
-  },  
-  // Add node events  
-  Events: {  
-    enable: true,  
-    //Change cursor style when hovering a node  
-    onMouseEnter: function() {  
-      fd.canvas.getElement().style.cursor = 'move';  
-    },  
-    onMouseLeave: function() {  
-      fd.canvas.getElement().style.cursor = '';  
-    },  
-    //Update node positions when dragged  
-    onDragMove: function(node, eventInfo, e) {  
-		var pos = eventInfo.getPos();  
-		node.pos.setc(pos.x, pos.y);  
-		fd.plot();  
-    },  
-    //Implement the same handler for touchscreens  
-    onTouchMove: function(node, eventInfo, e) {  
-      $jit.util.event.stop(e); //stop default touchmove event  
-      this.onDragMove(node, eventInfo, e);  
-    },  
-    //Add also a click handler to nodes  
-    onClick: function(node) {  
-      if(!node) return;  
-      // Build the right column relations list.  
-      // This is done by traversing the clicked node connections.  
-      //var html = "<h4>" + node.name + " (" + node.data.structure + ") </h4><b> prob. association:</b>" + node.data.prob_association;  
-      //append connections information  
-      //$jit.id('inner-details').innerHTML = html;  
-    }  
-  },  
-  //Number of iterations for the FD algorithm  
-  iterations: 200,  
-  //Edge length  
-  levelDistance: 375,  
-  // Add text to the labels. This method is only triggered  
-  // on label creation and only for DOM labels (not native canvas ones).  
-  onCreateLabel: function(domElement, node){  
-    domElement.innerHTML = node.name;  
-    var style = domElement.style;  
-    style.fontSize = "1em";  
-    style.color = "#333";  
-  },  
-  // Change node styles when DOM labels are placed  
-  // or moved.  
-  onPlaceLabel: function(domElement, node){  
-    var style = domElement.style;  
-    var left = parseInt(style.left);  
-    var top = parseInt(style.top);  
-    var w = domElement.offsetWidth;  
-    style.left = (left - w / 2) + 'px';  
-    style.top = (top + 10) + 'px';  
-    style.display = '';  
-  }  
+	$("#infovis").empty();
+	fd = new $jit.ForceDirected({  
+	//id of the visualization container  
+	injectInto: 'infovis',  
+	//Enable zooming and panning  
+	//by scrolling and DnD  
+	Navigation: {  
+		enable: true,  
+		//Enable panning events only if we're dragging the empty  
+		//canvas (and not a node).  
+		panning: 'avoid nodes',  
+		zooming: 10 //zoom speed. higher is more sensible  
+	},  
+	// Change node and edge styles such as  
+	// color and width.  
+	// These properties are also set per node  
+	// with dollar prefixed data-properties in the  
+	// JSON structure.  
+	Node: {  
+		overridable: true  
+	},  
+	Edge: {  
+		overridable: true,  
+		color: '#23A4FF',  
+		lineWidth: 0.4  
+	},  
+	//Native canvas text styling  
+	Label: {  
+		type: labelType, //Native or HTML  
+		size: 16,  
+		style: 'normal',
+		color: '#000'
+	},  
+	//Add Tips  
+	Tips: {  
+		enable: true,  
+		onShow: function(tip, node) {  
+			//count connections  
+			var count = 0;  
+			node.eachAdjacency(function() { count++; });  
+			//display node info in tooltip  
+			tip.innerHTML = "<div class=\"tip-text\"><b>Category:</b> " + node.data.category + "<br /><b>Prob. association with search term:</b> " + node.data.prob_association + "</div>";  
+		}  
+	},  
+	// Add node events  
+	Events: {  
+		enable: true,  
+		//Change cursor style when hovering a node  
+		onMouseEnter: function() {  
+			fd.canvas.getElement().style.cursor = 'move';  
+		},  
+		onMouseLeave: function() {  
+			fd.canvas.getElement().style.cursor = '';  
+		},  
+		//Update node positions when dragged  
+		onDragMove: function(node, eventInfo, e) {  
+			var pos = eventInfo.getPos();  
+			node.pos.setc(pos.x, pos.y);  
+			fd.plot();  
+		},  
+		//Implement the same handler for touchscreens  
+		onTouchMove: function(node, eventInfo, e) {  
+			$jit.util.event.stop(e); //stop default touchmove event  
+			this.onDragMove(node, eventInfo, e);  
+		},  
+	},  
+	//Number of iterations for the FD algorithm  
+	iterations: 200,  
+	//Edge length  
+	levelDistance: 375,  
+	// Add text to the labels. This method is only triggered  
+	// on label creation and only for DOM labels (not native canvas ones).  
+	onCreateLabel: function(domElement, node){  
+		domElement.innerHTML = node.name;  
+		var style = domElement.style;  
+		style.fontSize = "1em";  
+		style.color = "#333";  
+	},  
+	// Change node styles when DOM labels are placed  
+	// or moved.  
+	onPlaceLabel: function(domElement, node){  
+		var style = domElement.style;  
+		var left = parseInt(style.left);  
+		var top = parseInt(style.top);  
+		var w = domElement.offsetWidth;  
+		style.left = (left - w / 2) + 'px';  
+		style.top = (top + 10) + 'px';  
+		style.display = '';  
+	}  
 });  
 // load JSON data.  
 fd.loadJSON(json);  
 // compute positions incrementally and animate.  
 fd.computeIncremental({  
-  iter: 40,  
-  property: 'end',  
-  onStep: function(perc){  
-    //Log.write(perc + '% loaded...');  
-  },  
-  onComplete: function(){  
-    //Log.write('done');  
-    fd.animate({  
-      modes: ['linear'],  
-      transition: $jit.Trans.Elastic.easeOut,  
-      duration: 2500  
-    });  
-  }  
+	iter: 40,  
+	property: 'end',  
+	onComplete: function(){  
+		fd.animate({  
+			modes: ['linear'],  
+			transition: $jit.Trans.Elastic.easeOut,  
+			duration: 2500  
+		});  
+	}  
 });
 }
 
-function removeCategory(cat)
-{
+function removeCategory(cat) {
 	var stuffToRemove = [];
 	
 	fd.graph.eachNode(function(node) {  
-	  if (node.data.category == cat)
-		stuffToRemove.push(node.id);
-  	});
+		if (node.data.category == cat)
+			stuffToRemove.push(node.id);
+	});
   	
-  	fd.op.removeNode(stuffToRemove);
+	fd.op.removeNode(stuffToRemove);
   	
-  	fd.animate();
+	fd.animate();
 }
 
-function init(json)
-{
+function init(json) {
 	makeGraph(json);
 	
 	$("#removeStructures").click(function(){
